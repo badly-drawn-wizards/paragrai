4e928ec042cf30df9abfb0830be452e82bc89e56
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2022-11-11 19:54:26 +0200

    Work towards rest api

--- paragrai.py
+++ paragrai.py
@@ -4,15 +4,15 @@ from transformers import pipeline, PreTrainedTokenizer, \
   LogitsProcessor, LogitsWarper
 import numpy as np
 import torch as pt
+import logging as log
 from collections import defaultdict
 from dataclasses import dataclass, replace
 from typing import *
 from math import ceil
 from itertools import chain
 from sys import argv
-
-generator = pipeline('text-generation', model='distilgpt2')
-tokenizer = generator.tokenizer
+from os.path import join
+from hashlib import sha256
 
 def get_word_mask(tokens, decode):
   return np.array([decode(token).startswith(' ') for token in tokens])
@@ -24,14 +24,11 @@ class ParagraphLogitsProcessor(LogitsProcessor):
   word_mask: pt.BoolTensor
   eos_token_id: int
   linebreaks: List[int]
-  para_config: Dict[str, Any]
+  min_space: Dict[str, Any]
   lb_weight: float
 
   def num_batches(self):
       return ceil(len(self.tokens)/self.batch_size)
-
-  def min_space(self):
-    return self.para_config['min_space']
 
   def __call__(self, input_ids: pt.LongTensor,
                scores: pt.FloatTensor) -> pt.FloatTensor:
@@ -54,7 +51,7 @@ class ParagraphLogitsProcessor(LogitsProcessor):
     trunc_ixs = pt.minimum(token_ixs, pt.tensor(len(self.tokens)-1))
     last_lbs, _ = pt.max(lbs * pt.arange(n).T, dim=1)
 
-    can_lb = pt.argwhere((self.min_space() < n - last_lbs)
+    can_lb = pt.argwhere((self.min_space < n - last_lbs)
       & self.word_mask[trunc_ixs])
 
     complete = pt.argwhere(batch_ixs == batch_sizes)
@@ -85,22 +82,55 @@ def main():
     with open(in_path) as f:
         sample = f.read()
 
-    batch_size=256
+
+    with open(out_path, 'w') as f:
+        f.write(out)
+
+@dataclass
+class ParagraiConfig:
+  model: str = 'distilgpt2'
+  min_space: int = 1
+  batch_size: int = 256
+  lb_weight: float = 3.6
+
+class Cache:
+  def __init__(self, func, path, hash=sha256):
+    self.func = func
+    self.path = path
+    self.hash = hash
+
+  def lookup(self, content):
+    digest = self.hash(content.encode()).hexdigest()
+    try:
+      with open(join(self.path, digest), 'r') as f:
+        return f.read()
+    except FileNotFoundError:
+      result = self.func(content)
+      try:
+        with open(join(self.path, digest), 'w') as f:
+          f.write(content)
+      except:
+        log.exception("Error when writing to cache")
+      return result
+
+class ParagraiGenerator:
+  def __init__(self, config):
+    self.config = config
+    self.generator = pipeline('text-generation', self.config.model)
+
+  def generate(self, sample):
+    tokenizer = self.generator.tokenizer
     tokens = pt.squeeze(tokenizer(sample, return_tensors='pt')['input_ids'], dim=0)
-    # padding = (batch_size - len(tokens)%batch_size) % batch_size
-    # pad_tokens = pt.hstack([tokens, pt.tensor(tokenizer.eos_token_id).repeat(padding)])
 
-    word_mask = get_word_mask(tokens, tokenizer.decode)
     linebreaks = np.squeeze(tokenizer(["\n", "\n\n"])['input_ids'], axis=1)
-    para_config = {
-        'min_space': 1
-    }
-    lb_weight = 3.6
-    para_logits_processor = ParagraphLogitsProcessor(
-        tokens, batch_size, word_mask, tokenizer.eos_token_id,
-        linebreaks, para_config, lb_weight)
+    
+    word_mask = get_word_mask(tokens, tokenizer.decode)
 
-    results = generator(
+    para_logits_processor = ParagraphLogitsProcessor(
+        tokens, self, word_mask, tokenizer.eos_token_id,
+        linebreaks, self.config.min_space, self.config.lb_weight)
+
+    results = self.generator(
         "",
         logits_processor=[para_logits_processor],
         return_tensors=True,
@@ -111,10 +141,4 @@ def main():
         num_beams=2)
 
     out_tokens = list(chain(*[result['generated_token_ids'] for result in results]))
-    out = tokenizer.decode(out_tokens, skip_special_tokens=True)
-
-    with open(out_path, 'w') as f:
-        f.write(out)
-
-if __name__ == "__main__":
-    main()
+    return tokenizer.decode(out_tokens, skip_special_tokens=True)
